54163d2ce8c5036b30061b622a55fc0b0beb43ec
jonathan laasonen <jonathan.laasonen@appliedtechnology.se>
2019-11-27 09:40:30 +0100

    update info and feedscreen

--- Client/components/FeedCard.js
+++ Client/components/FeedCard.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View, Text, StyleSheet, Dimensions } from 'react-native';
 
-const FeedCard = ({data:{item:{data:{message, title}}}}) => {
+const FeedCard = ({ data: { item: { data: { message, title } } } }) => {
 
   return (
     <View style={styles.wrapper}>
-      <Text style={styles.title}>{title}</Text>
-      <Text style={styles.message}>{message}</Text>
+      <View style={styles.iconBg}><Text style={styles.icon}>{title.charAt(0)}</Text></View>
+      <View style={styles.pushMessage}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.message}>{message}</Text>
+      </View>
     </View>
   )
 }
@@ -15,8 +18,10 @@ const styles = StyleSheet.create({
   wrapper: {
     paddingBottom: 30,
     paddingTop: 23,
+    paddingLeft: 10,
     borderBottomWidth: 0.5,
-    width: Dimensions.get('window').width - 40
+    width: Dimensions.get('window').width - 40,
+    flexDirection: 'row'
   },
   title: {
     fontWeight: '600',
@@ -25,7 +30,26 @@ const styles = StyleSheet.create({
   },
   message: {
     paddingLeft: 10
+  },
+  iconBg:
+  {
+    borderRadius: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 70,
+    height: 70,
+    backgroundColor: '#DF8187'
+  },
+  icon:
+  {
+    fontSize: 36,
+    color: 'white',
+    fontWeight: '900'
+  },
+  pushMessage: {
+    justifyContent: 'center'
   }
+
 })
 
 export default FeedCard
